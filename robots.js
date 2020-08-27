26aaf4141d1e9b466d2796bc4105953e19d56cc2
Abhay Goyal <66846495+Codebat-man@users.noreply.github.com>
2020-08-27 17:44:18 +0530

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Ayush ",
+    username: "a_m",
+    email: "amittalb=4_be19@thapar.edu",
+  },
+  {
+    id: 2,
+    name: "Sugam",
+    username: "Sugam Baalo Wala",
+    email: "sgoyal4_be19@thapar.edu",
+  },
+  {
+    id: 3,
+    name: "Uday",
+    username: "Dropper_Uday",
+    email: "uday4_be19@thapar.edu",
+  },
+  {
+    id: 4,
+    name: "Rakshit",
+    username: "ccs WALE",
+    email: "rakshit4_be19@thapar.edu",
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+  },
+];
